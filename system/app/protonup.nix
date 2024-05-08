9fde1e3df2c6cd9378b22483191155834fe7999c
kevindherdt <kevindherdt@hotmail.com>
2024-05-08 14:37:52 +0200

    try get protonge working

--- system/app/protonup.nix
+++ system/app/protonup.nix
@@ -1,8 +1,16 @@
+# configuration.nix
+
 { pkgs, ... }:
 
 {
+
   environment.systemPackages = with pkgs; [
     protonup
   ];
+  
+  home.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+  };
 
 }
