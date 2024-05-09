155ac0c1cdb3642cf0dec9773d44a67d16a74d21
kevindherdt <kevindherdt@hotmail.com>
2024-05-09 22:37:38 +0200

    Fixing stupid newbie mistakes from copying in terminal

--- home.nix
+++ home.nix
@@ -18,9 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-  pkgs.steam
-  pkgs.lutris
-  pkgs.heroic
   
   # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
