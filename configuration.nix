6688e148a304bd0edbcbee949e1283a8fc20f72d
kevindherdt <kevindherdt@hotmail.com>
2024-05-15 12:35:39 +0200

    Added my drive to boot automatically

--- configuration.nix
+++ configuration.nix
@@ -47,6 +47,11 @@
  # virtualisation.libvirtd.enable = true;
  # programs.virt-manager.enable = true;
 
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/fc337e71-6213-429d-aeb7-38ff3dc35aa6";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
 
  # Enable CUPS to print documents.
  # services.printing.enable = true;
